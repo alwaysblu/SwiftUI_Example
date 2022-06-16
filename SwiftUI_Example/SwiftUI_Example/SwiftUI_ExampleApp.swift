6bc7e4c9f0d05592253b3e0fcacc38148a6283e3
alwaysblu <stepsw112@gmail.com>
2022-06-16 17:34:47 +0900

    🎉 [Begin] : SwiftUI_Example

---
+++ SwiftUI_Example/SwiftUI_Example/SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExampleApp.swift
+//  SwiftUI_Example
+//
+//  Created by 맥북 on 2022/06/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
