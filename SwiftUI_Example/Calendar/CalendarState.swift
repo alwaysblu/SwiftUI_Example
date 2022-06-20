92e128c7790d1d14328e2efa275e2f26f9e65f44
alwaysblu <stepsw112@gmail.com>
2022-06-21 05:56:33 +0900

    add logic

--- SwiftUI_Example/Calendar/CalendarState.swift
+++ SwiftUI_Example/Calendar/CalendarState.swift
@@ -9,35 +9,44 @@ import ComposableArchitecture
 import CoreGraphics
 
 struct CalendarState: Equatable {
-  var year: Int
-  var month: Int
-  var totalGrid: [DayInformation]
-  var currentDay: Int?
-  var itemSize: CGSize = .zero
-  var isDatePickerShowUp: Bool
-
-  init(
-    year: Int,
-    month: Int,
-    totalGrid: [DayInformation] = [],
-    currentDay: Int? = nil,
-    itemSize: CGSize = .zero,
-    isDatePickerShowUp: Bool = false
-  ) {
-    self.year = year
-    self.month = month
-    self.totalGrid = Array(0...30)
-      .map {
-        let heartCount = Int.random(in: 0...2)
-        return DayInformation(
-          day: $0,
-          heartCount: Int.random(in: 0...2),
-          isOrderDay: Bool.random(),
-          isHeartExist: heartCount == 0 ? false : true
-        )
-      }
-    self.currentDay = currentDay
-    self.itemSize = itemSize
-    self.isDatePickerShowUp = isDatePickerShowUp
-  }
+    var year: Int
+    var month: Int
+    var totalGrid: [DayInformation]
+    var currentDay: Int?
+    var itemSize: CGSize = .zero
+    var isDatePickerShowUp: Bool
+    
+    init(
+        year: Int,
+        month: Int,
+        totalGrid: [DayInformation] = [],
+        currentDay: Int? = nil,
+        itemSize: CGSize = .zero,
+        isDatePickerShowUp: Bool = false
+    ) {
+        let today = Date()
+        let year = Calendar.current.component(.year, from: today)
+        let month = Calendar.current.component(.month, from: today)
+        let day = Calendar.current.component(.day, from: today)
+        let weekday = Calendar.current.component(.weekday, from: today)
+        
+        self.year = year
+        self.month = month
+        self.totalGrid = Array(1...day)
+            .map {
+                let heartCount = Int.random(in: 0...2)
+                return DayInformation(
+                    day: $0,
+                    heartCount: Int.random(in: 0...2),
+                    isOrderDay: Bool.random(),
+                    isHeartExist: heartCount == 0 ? false : true
+                )
+            }
+        for _ in 1...weekday {
+            self.totalGrid.insert(DayInformation(day: 0, heartCount: 0, isOrderDay: false, isHeartExist: false), at: 0)
+        }
+        self.currentDay = currentDay
+        self.itemSize = itemSize
+        self.isDatePickerShowUp = isDatePickerShowUp
+    }
 }
--- SwiftUI_Example/Calendar/CalendarView.swift
+++ SwiftUI_Example/Calendar/CalendarView.swift
@@ -11,162 +11,154 @@ import SwiftUI
 // MARK: View
 
 struct CalendarView: View {
-
-  @ObservedObject
-  private var viewStore: CalendarViewStore
-
-  private let store: CalendarStore
-  private let dayColumns: [GridItem]
-
-
-  init(store: CalendarStore) {
-    self.viewStore = ViewStore(store)
-    self.store = store
-    self.dayColumns = [
-      GridItem(.flexible(minimum: 20, maximum: 100)),
-      GridItem(.flexible(minimum: 20, maximum: 100)),
-      GridItem(.flexible(minimum: 20, maximum: 100)),
-      GridItem(.flexible(minimum: 20, maximum: 100)),
-      GridItem(.flexible(minimum: 20, maximum: 100)),
-      GridItem(.flexible(minimum: 20, maximum: 100)),
-      GridItem(.flexible(minimum: 20, maximum: 100))
-    ]
-  }
-
-  var body: some View {
-    VStack {
-      datePickerButton
-      dayOfWeekGrid
-      ScrollView(showsIndicators: false) {
-        Spacer(minLength: 15)
-        dayGrid
-      }
-      .frame(width: nil, height: 150, alignment: .center)
-      .fixedSize(horizontal: false, vertical: true)
+    
+    @ObservedObject
+    private var viewStore: CalendarViewStore
+    
+    private let store: CalendarStore
+    private let dayColumns: [GridItem]
+    
+    
+    init(store: CalendarStore) {
+        self.viewStore = ViewStore(store)
+        self.store = store
+        self.dayColumns = [
+            GridItem(.flexible(minimum: 20, maximum: 100)),
+            GridItem(.flexible(minimum: 20, maximum: 100)),
+            GridItem(.flexible(minimum: 20, maximum: 100)),
+            GridItem(.flexible(minimum: 20, maximum: 100)),
+            GridItem(.flexible(minimum: 20, maximum: 100)),
+            GridItem(.flexible(minimum: 20, maximum: 100)),
+            GridItem(.flexible(minimum: 20, maximum: 100))
+        ]
     }
-    .padding(.horizontal, 15)
-    .padding(.vertical, 20)
-    .background(.white)
-  }
-
-//  private var datePicker: some View {
-//    DatePicker(
-//      "",
-//      selection: viewStore.$date,
-//      displayedComponents: [.date]
-//    )
-//    .background(.white)
-//    .datePickerStyle(.compact)
-//  }
-
-  private var datePickerButton: some View {
-    HStack {
-      Button {
-        viewStore.send(.showDatePickerView)
-      } label: {
-        HStack() {
-          Text("\(viewStore.year) \(viewStore.month)")
-            .foregroundColor(.gray)
-          Image("arrow")
+    
+    var body: some View {
+        VStack {
+            datePickerButton
+            dayOfWeekGrid
+            ScrollView(showsIndicators: false) {
+                Spacer(minLength: 15)
+                dayGrid
+            }
+            .frame(width: nil, height: 150, alignment: .center)
+            .fixedSize(horizontal: false, vertical: true)
         }
-      }
-      Spacer()
+        .padding(.horizontal, 15)
+        .padding(.vertical, 20)
+        .background(.white)
     }
-  }
-
-  private var dayOfWeekGrid: some View {
-    LazyVGrid(
-      columns: dayColumns
-    ) {
-      ForEach(DayOfWeek.allCases, id: \.self) { dayOfWeek in
-        Text(dayOfWeek.description)
-          .font(.body)
-      }
-    }
-    .padding(.horizontal, 5)
-    .background(.white)
-  }
-
-  private var dayGrid: some View {
-    LazyVGrid(
-      columns: dayColumns,
-      spacing: 15
-    ) {
-      ForEach(viewStore.totalGrid, id: \.self) { day in
-        makeDayView(day)
-      }
-    }.padding(.horizontal, 5)
-  }
-
-  private func makeDayView(_ data: DayInformation) -> some View {
-    VStack(spacing: 6) {
-      ZStack {
-        if viewStore.currentDay == data.day {
-          Circle()
-            .scale(2)
-            .fill(.blue)
-        }
+    
+    private var datePickerButton: some View {
         HStack {
-          if data.isOrderDay {
-            Image("cart")
-          }
-          Spacer()
+            Button {
+                viewStore.send(.showDatePickerView)
+            } label: {
+                HStack() {
+                    Text(String(describing: viewStore.year) + " " + viewStore.month.month)
+                        .foregroundColor(.gray)
+                    Image("arrow")
+                }
+            }
+            Spacer()
         }
-        Text(String(format: "%02d", data.day))
-          .foregroundColor(.black)
-          .font(.subheadline)
-      }
-      if data.isHeartExist {
-        HStack(spacing: 2) {
-          Image("heart")
-            .resizable()
-            .frame(width: 10, height: 10, alignment: .center)
-          Text("\(data.heartCount)")
-            .font(.caption2)
+    }
+    
+    private var dayOfWeekGrid: some View {
+        LazyVGrid(
+            columns: dayColumns
+        ) {
+            ForEach(DayOfWeek.allCases, id: \.self) { dayOfWeek in
+                Text(dayOfWeek.description)
+                    .font(.body)
+            }
         }
-        .frame(
-          width: 25,
-          height: 12,
-          alignment: .center
-        )
-      } else {
-        Spacer(minLength: 12)
-      }
-    }.onTapGesture {
-      viewStore.send(.setCurrentDay(data.day))
-    }.frame(width: 40)
-  }
+        .padding(.horizontal, 5)
+        .background(.white)
+    }
+    
+    private var dayGrid: some View {
+        LazyVGrid(
+            columns: dayColumns,
+            spacing: 15
+        ) {
+            ForEach(viewStore.totalGrid, id: \.self) { day in
+                makeDayView(day)
+            }
+        }.padding(.horizontal, 5)
+    }
+    
+    private func makeDayView(_ data: DayInformation) -> some View {
+        VStack(spacing: 6) {
+            ZStack {
+                if viewStore.currentDay == data.day {
+                    Circle()
+                        .scale(2)
+                        .fill(.blue)
+                }
+                HStack {
+                    if data.isOrderDay {
+                        Image("cart")
+                    }
+                    Spacer()
+                }
+                if data.day > 0 {
+                    Text(String(format: "%02d", data.day))
+                        .foregroundColor(.black)
+                        .font(.subheadline)
+                }
+            }
+            if data.isHeartExist {
+                HStack(spacing: 2) {
+                    Image("heart")
+                        .resizable()
+                        .frame(width: 10, height: 10, alignment: .center)
+                    Text("\(data.heartCount)")
+                        .font(.caption2)
+                }
+                .frame(
+                    width: 25,
+                    height: 12,
+                    alignment: .center
+                )
+            } else {
+                Spacer(minLength: 12)
+            }
+        }.onTapGesture {
+            viewStore.send(.setCurrentDay(data.day))
+        }.frame(width: 40)
+    }
 }
 
 // MARK: Store
 
 typealias CalendarStore = Store<
-  CalendarState,
-  CalendarAction
+    CalendarState,
+    CalendarAction
 >
 
 // MARK: ViewStore
 
 typealias CalendarViewStore = ViewStore<
-  CalendarState,
-  CalendarAction
+    CalendarState,
+    CalendarAction
 >
 
 // MARK: Preview
 
 struct CalendarView_Previews: PreviewProvider {
-
-  static var previews: some View {
-    ForEach(ColorScheme.allCases, id: \.self) { colorScheme in
-      CalendarView(store: store)
-        .preferredColorScheme(colorScheme)
-        .previewLayout(.sizeThatFits)
+    
+    static var previews: some View {
+        ForEach(ColorScheme.allCases, id: \.self) { colorScheme in
+            CalendarView(store: store)
+                .preferredColorScheme(colorScheme)
+                .previewLayout(.sizeThatFits)
+        }
     }
-  }
-
-  static let store: CalendarStore = .init(
-    initialState: .init(year: 2022, month: 2),
-    reducer: .init(),
-    environment: .init()
-  )
+    
+    static let store: CalendarStore = .init(
+        initialState: .init(year: 2022, month: 2),
+        reducer: .init(),
+        environment: .init()
+    )
 }
